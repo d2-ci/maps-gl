6139e726a874d22e300e68f3b731a8d9382828f1
Jen Jones Arnesen <jennifer@dhis2.org>
2024-07-03 08:35:16 +0000

    e567619 chore(deps): bump the dependencies group with 9 updates (#578)

--- build/cjs/layers/Spider.js
+++ build/cjs/layers/Spider.js
@@ -7,7 +7,7 @@ exports.default = void 0;
 var _maplibreGl = require("maplibre-gl");
 var _spiderifier = _interopRequireDefault(require("../utils/spiderifier"));
 var _style = require("../utils/style");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 const Spider = function (map, options) {
   let spider;
   let spiderId;
