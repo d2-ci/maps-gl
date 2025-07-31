34b5cf2e1a94cf7c0688d1c04cd2d3f661923bba
GitHub <noreply@github.com>
2025-07-31 08:43:05 +0000

    54733dc chore: add comment to index.js

--- build/cjs/earthengine/index.js
+++ build/cjs/earthengine/index.js
@@ -7,4 +7,5 @@ exports.default = void 0;
 var _ee_worker_loader = _interopRequireDefault(require("./ee_worker_loader.js"));
 require("./ee_worker.js");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+// Ensure Vite can resolve this file when this package is symlinked via `yarn link` into another project
 var _default = exports.default = _ee_worker_loader.default;
