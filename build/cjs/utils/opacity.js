02e4fef891659cbce79b056d2ee0c2a426a2ae3b
GitHub <noreply@github.com>
2026-01-23 14:36:32 +0000

    7780c8d chore(deps): bump lodash in the security group across 1 directory (#626)

---
+++ build/cjs/utils/opacity.js
@@ -0,0 +1,34 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.setLayersOpacity = void 0;
+const properties = {
+  raster: ['raster-opacity'],
+  point: ['circle-opacity', 'circle-stroke-opacity'],
+  polygon: ['fill-opacity'],
+  line: ['line-opacity'],
+  outline: ['line-opacity'],
+  buffer: ['fill-opacity'],
+  'buffer-outline': ['line-opacity'],
+  label: ['text-opacity'],
+  symbol: ['icon-opacity', 'text-opacity'],
+  cluster: ['circle-opacity', 'circle-stroke-opacity'],
+  count: ['text-opacity']
+};
+const opacityFactor = {
+  buffer: 0.2,
+  'buffer-outline': 0.2
+};
+const getOpacity = (key, opacity) => opacity * (opacityFactor[key] || 1);
+const setLayersOpacity = (mapgl, id, opacity) => {
+  mapgl.getStyle().layers.filter(layer => layer.id.startsWith(id)).forEach(layer => {
+    const key = layer.id.split('-').pop();
+    const value = getOpacity(key, opacity);
+    properties[key]?.forEach(property => {
+      mapgl.setPaintProperty(layer.id, property, value);
+    });
+  });
+};
+exports.setLayersOpacity = setLayersOpacity;
