617a420489a40d7e65d975b551947069e0976973
Bruno Raimbault <bruno@dhis2.org>
2026-02-05 16:58:56 +0000

    98d64e5 fix: rollback turf update

---
+++ build/es/utils/filters.js
@@ -0,0 +1,13 @@
+const getCluster = ['get', 'cluster'];
+const getIsPolygon = ['get', 'isPolygon'];
+const hasImage = ['has', 'iconUrl'];
+export const isPoint = ['==', ['geometry-type'], 'Point'];
+export const isPolygon = ['==', ['geometry-type'], 'Polygon'];
+export const isLine = ['==', ['geometry-type'], 'LineString'];
+export const isSymbol = ['all', isPoint, hasImage];
+export const isPointNoSymbol = ['all', isPoint, ['==', hasImage, false]];
+export const isCluster = ['==', getCluster, true];
+export const noCluster = ['!=', getCluster, true];
+export const isClusterPoint = ['all', isPoint, noCluster, ['!=', getIsPolygon, true]];
+export const isClusterPolygon = ['all', noCluster, ['==', getIsPolygon, true]];
+export const isHover = ['boolean', ['feature-state', 'hover'], false];
